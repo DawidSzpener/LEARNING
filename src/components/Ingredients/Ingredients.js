3d1c9f634968675430e37aeba8fbb4cbee2b349e
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-08 09:49:58 +0000

    Change ingredients state using useState hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from './IngredientList'
 
-function Ingredients() {
+const Ingredients = () => {  
+  const [userIngredients, setUserIngredients] = useState([])
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ])
+  }
+
+  const removeIngredientHandler = (id) => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients.filter(ig => 
+        ig.id !== id)
+    ])
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
   );
