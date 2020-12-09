12ac284bf510fc12576aa00ff39afd85af49a910
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-09 13:35:03 +0000

    Fetch ingredients with useEffect hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,6 +6,22 @@ import IngredientList from './IngredientList'
 
 const Ingredients = () => {  
   const [userIngredients, setUserIngredients] = useState([])
+
+  useEffect(() => {
+    fetch('https://react-hooks-8d547-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = []
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          })
+        }
+        setUserIngredients(loadedIngredients)
+      })
+  }, [])
 
   const addIngredientHandler = (ingredient) => {
     fetch('https://react-hooks-8d547-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json', {
